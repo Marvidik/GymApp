b5b945bab4d17e69155fae3ab99814990c28db66
Marvidik <ebubeidika@gmail.com>
2023-09-06 20:38:06 +0100

    working on the text input component

---
+++ Components/TextInputComp.js
@@ -0,0 +1,15 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function TextInputComp() {
+  return (
+    <View>
+      <Text>TextInputComp</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+
+})
